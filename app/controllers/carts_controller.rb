916f448084ff71743e9684ae8728ff7a51e5f4f7
Maciej <mgalazka@mimuw.edu.pl>
2019-05-07 18:48:56 +0200

    Erase a few files

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
   def show
+    render_to_string
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render :index
   end
 end
